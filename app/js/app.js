8d68318778b248866654322139d76be582017894
Jeannie An <jeanniehjan@gmail.com>
2020-12-31 23:16:34 +0000

    working on fetch method

--- app/js/app.js
+++ app/js/app.js
@@ -1,32 +1,51 @@
+// Create Dino Constructor
 
-    // Create Dino Constructor
+function Dino(name, height, weight, diet) {
+  this.name = name;
+  this.height = height;
+  this.weight = weight;
+  this.diet = diet;
+}
 
+function fetchDinoData() {
+  let arr = [];
 
-    // Create Dino Objects
+  fetch("../dino.json")
+    .then((response) => response.json())
+    .then((data) => {
+      arr = data;
+      console.log("then");
+      console.log(arr);
+      return arr;
+    })
+    .catch((error) => console.log(error.message))
+    .finally(() => {
+      console.log("Finally", arr);
+    });
+}
 
+let dinos = fetchDinoData();
+console.log("dinos", dinos);
 
-    // Create Human Object
+// Create Dino Objects
 
-    // Use IIFE to get human data from form
+// Create Human Object
 
+// Use IIFE to get human data from form
 
-    // Create Dino Compare Method 1
-    // NOTE: Weight in JSON file is in lbs, height in inches. 
+// Create Dino Compare Method 1
+// NOTE: Weight in JSON file is in lbs, height in inches.
 
-    
-    // Create Dino Compare Method 2
-    // NOTE: Weight in JSON file is in lbs, height in inches.
+// Create Dino Compare Method 2
+// NOTE: Weight in JSON file is in lbs, height in inches.
 
-    
-    // Create Dino Compare Method 3
-    // NOTE: Weight in JSON file is in lbs, height in inches.
+// Create Dino Compare Method 3
+// NOTE: Weight in JSON file is in lbs, height in inches.
 
+// Generate Tiles for each Dino in Array
 
-    // Generate Tiles for each Dino in Array
-  
-        // Add tiles to DOM
+// Add tiles to DOM
 
-    // Remove form from screen
-
+// Remove form from screen
 
 // On button click, prepare and display infographic
